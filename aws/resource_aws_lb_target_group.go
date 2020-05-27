415d20230bc9dec50cd80a3f088de831bda5137f
Nick Venenga <nijave@gmail.com>
2020-05-27 08:46:13 -0400

    Revert "Correctly restore Target Group LB algorithm default"

--- aws/resource_aws_lb_target_group.go
+++ aws/resource_aws_lb_target_group.go
@@ -124,7 +124,7 @@ func resourceAwsLbTargetGroup() *schema.Resource {
 			"load_balancing_algorithm_type": {
 				Type:     schema.TypeString,
 				Optional: true,
-				Default:  "round_robin",
+				Computed: true,
 				ValidateFunc: validation.StringInSlice([]string{
 					"round_robin",
 					"least_outstanding_requests",
